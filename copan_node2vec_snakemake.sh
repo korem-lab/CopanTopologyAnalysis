2475e1beb88fccd392b3ba1235f2f844fbdb2d14
Rebecca Christensen <rc3710@m001.eth.cluster>
2024-10-25 16:40:16 -0400

    hi

--- copan_node2vec_snakemake.sh
+++ copan_node2vec_snakemake.sh
@@ -3,9 +3,9 @@
 #SBATCH --job-name=copanmake
 #SBATCH --output=workflow/out/job_out/copanmake_%j.out  # Standard output
 #SBATCH --error=workflow/out/job_out/copanmake_%j.err    # Standard error
-#SBATCH --time=05:00:00
+#SBATCH --time=48:00:00
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=2
 #SBATCH --mem=20G
 #SBATCH --account pmg
 
@@ -15,4 +15,4 @@ conda activate snakemake
 cd /burg/pmg/users/rc3710/CopanTopologyAnalysis
 
 snakemake --unlock
-snakemake -c 1 -j 1 --rerun-incomplete --keep-going
+snakemake -c 2 -j 2 --rerun-incomplete --keep-going
