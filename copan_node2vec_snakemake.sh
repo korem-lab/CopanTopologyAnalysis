a54da817638f9d786b9afe5886c46c98492f9bed
RebbyAnne <rebbyanne@gmail.com>
2024-11-05 14:52:27 -0500

    hi

--- copan_node2vec_snakemake.sh
+++ copan_node2vec_snakemake.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 #
 #SBATCH --job-name=copanmake
-#SBATCH --output=workflow/out/job_out/copanmake_%j.out  # Standard output
-#SBATCH --error=workflow/out/job_out/copanmake_%j.err    # Standard error
+#SBATCH --output=job_out/copanmake_%j.out  # Standard output
+#SBATCH --error=job_out/copanmake_%j.err    # Standard error
 #SBATCH --time=48:00:00
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=2
+#SBATCH --ntasks-per-node=1
 #SBATCH --mem=20G
 #SBATCH --account pmg
 
-source /burg/pmg/users/korem_lab/miniforge3/etc/profile.d/conda.sh  # Ensures conda is available in the script
+source /burg/pmg/users/korem_lab/miniforge3/etc/profile.d/conda.sh
 conda activate snakemake
 
 cd /burg/pmg/users/rc3710/CopanTopologyAnalysis
 
 snakemake --unlock
-snakemake -c 2 -j 2 --rerun-incomplete
+snakemake -c 1 -j 1 --rerun-incomplete
