f328db6308f6ec55c4c039ff1419e99fa12d5512
RebbyAnne <rebbyanne@gmail.com>
2024-11-06 16:29:28 -0500

    hi

--- workflow/scripts/get_fasta_header.py
+++ workflow/scripts/get_fasta_header.py
@@ -8,15 +8,19 @@ header_f = "workflow/out/reference_metagenomes/sample_1_reference_metagenome_ids
 def main():
     with open(fasta_f, 'r') as infile:
         with open(header_f, 'w') as outfile:
+            outfile.write("seqid;genome;info")
             for line in infile:
                 if line.startswith(">"):
                     header = line[1:].strip()
                     header = header.split(" ", 1)
 
                     seq_id = str(header[0])
-                    description = str(header[1])
+                    description = header[1].split(",", 1)
 
-                    outfile.write(seq_id + ";" + description + "\n")
+                    genome = description[0]
+                    info = description[1]
+
+                    outfile.write(seq_id + ";" + genome + ";" + info + "\n")
 
 
 if __name__ == '__main__':
