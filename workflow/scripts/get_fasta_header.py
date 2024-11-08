7471c8614d2961ca2d66bf1594f13e9f896e8033
RebbyAnne <rebbyanne@gmail.com>
2024-11-08 10:46:18 -0500

    Remove previously tracked R-related files

--- workflow/scripts/get_fasta_header.py
+++ workflow/scripts/get_fasta_header.py
@@ -1,30 +1,39 @@
-import sys
 
-# fasta_f = "dummy.fasta"
-# header_f = sys.argv[2]
-fasta_f = "workflow/out/reference_metagenomes/sample_1_reference_metagenome.fasta"
-header_f = "workflow/out/reference_metagenomes/sample_1_reference_metagenome_ids.txt"
+fasta_f = "workflow/out/copangraphs/sample_1_0_02.fasta"
+header_f = "workflow/out/copangraphs/sample_1_0_02_ids.txt"
 
+# # this for format of reference metagenomes fasta
+# def main():
+#     with open(fasta_f, 'r') as infile:
+#         with open(header_f, 'w') as outfile:
+#             outfile.write("seqid;genome;info\n")
+#             for line in infile:
+#                 if line.startswith(">"):
+#                     header = line[1:].strip()
+#                     header = header.split(" ", 1)
+
+#                     seq_id = str(header[0])
+#                     description = header[1].split(",", 1)
+
+#                     if len(description) > 1:
+#                         genome = description[0]
+#                         info = description[1]
+
+#                         outfile.write(seq_id + ";" + genome + ";" + info + "\n")
+                    
+#                     else:
+#                         outfile.write(seq_id + ";" + description[0] + ";NA" + "\n")
+
+# this for format of sample 1 copan fasta
 def main():
     with open(fasta_f, 'r') as infile:
         with open(header_f, 'w') as outfile:
-            outfile.write("seqid;genome;info\n")
+            outfile.write("qseqid\n")
             for line in infile:
                 if line.startswith(">"):
                     header = line[1:].strip()
-                    header = header.split(" ", 1)
-
-                    seq_id = str(header[0])
-                    description = header[1].split(",", 1)
-
-                    if len(description) > 1:
-                        genome = description[0]
-                        info = description[1]
-
-                        outfile.write(seq_id + ";" + genome + ";" + info + "\n")
                     
-                    else:
-                        outfile.write(seq_id + ";" + description[0] + ";NA" + "\n")
+                    outfile.write(header + "\n")
 
 
 if __name__ == '__main__':
