e60488d3a228332da484c05cfe96567382e57f23
RebbyAnne <rebbyanne@gmail.com>
2024-11-08 13:26:40 -0500

    hi

--- batch_blast_jobs.sh
+++ batch_blast_jobs.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=blast_batch
 #SBATCH --output=job_out/blast_batch_%j.out  # Standard output
 #SBATCH --error=job_out/blast_batch_%j.err   # Standard error
-#SBATCH --time=40:00:00
+#SBATCH --time=01:00:00
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=10G
 #SBATCH --account pmg
